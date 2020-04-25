4f6f5fc0af6aeb7bb000b0fefaa3d037477f372d
lukasjoc <jochamlu@gmail.com>
2020-04-25 18:55:31 +0200

    add docker files and build

--- dev.sh
+++ dev.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#docker build -t lukasjoc:debug -f docker/Dockerfile .
+#docker run --name lukasjoc_container -d -p 1313:80 lukasjoc:debug
+
 if ! [ -d node_modules ]; then
   npm install
 fi
